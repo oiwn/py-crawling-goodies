412a89a3973430c406812a1510b2b76abb221d28
Alex C <alex.grablab@icloud.com>
2020-02-07 23:24:21 +0700

    initial

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="py-crawling-goodies",
+    version="0.0.1",
+    author="istinspring",
+    author_email="istinspring@yandex.com",
+    description="Collection of tools usefult in web scraping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/istinspring/py-crawling-goodies",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
