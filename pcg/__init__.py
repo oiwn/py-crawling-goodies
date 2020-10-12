99ba05cc463e6fae92120f7b376c2a1551dbc382
oiwn <alex@imscraping.ninja>
2020-10-13 01:42:05 +0700

    easy import of singleton

--- pcg/__init__.py
+++ pcg/__init__.py
@@ -1,3 +1,5 @@
+"""Keep imports easy"""
 # flake8: noqa
 from .redis.filters import RedisSetFilter, RedisBucketFilter
 from .redis.queues import RedisJsonLIFOQueue
+from .patterns.singleton import Singleton
