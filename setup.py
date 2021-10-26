853daba21e93a3e416b33a08a7241c57a87dfeaa
oiwn <alex@imscraping.ninja>
2021-10-26 13:09:48 +0700

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-crawling-goodies",
-    version="0.0.6",
+    version="0.0.7",
     author="oiwn",
     author_email="",
     description="Collection of tools usefult in web scraping",
